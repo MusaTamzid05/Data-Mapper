b43b932e444e3e6f0fb9e835adab256d17ea03fa
MusaTamzid05 <tamzid101@gmail.com>
2022-09-20 01:46:26 +0600

    removed bride pattern and now using data mapper

--- lib/table.go
+++ lib/table.go
@@ -4,6 +4,8 @@ import (
 	"gorm.io/gorm"
 )
 
+// data mapper pattern => https://martinfowler.com/eaaCatalog/dataMapper.html
+
 type Person struct {
 	gorm.Model
 	Name string
@@ -34,15 +36,20 @@ func (mapper *PersonMapper) Add(person Person) error {
 }
 
 func (mapper *PersonMapper) Update(person Person) {
+	mapper.database.DB.Save(&person)
 
 }
 
-func (mapper *PersonMapper) Delete(id uint) {
+func (mapper *PersonMapper) Delete(person Person) error {
+	result := mapper.database.DB.Delete(&person)
+	return result.Error
 
 }
 
-func (mapper *PersonMapper) GetAll(id uint) []Person {
+func (mapper *PersonMapper) GetAll() []Person {
 	persons := []Person{}
+	mapper.database.DB.Find(&persons)
+
 	return persons
 
 }
--- main.go
+++ main.go
@@ -15,10 +15,40 @@ func main() {
 	}
 
 	db.Migrate()
-
 	personMapper := lib.NewPersonMapper(db)
-	personMapper.Add(lib.Person{Name: "Musa", Age: 27})
 
-	person2 := personMapper.Get(1)
-	fmt.Println(person2)
+	/*
+		personMapper.Add(lib.Person{Name: "Musa", Age: 27})
+
+		person2 := personMapper.Get(1)
+		fmt.Println(person2)
+
+		person2.Name = "Tamzid"
+		personMapper.Update(person2)
+
+		delPerson := personMapper.Get(7)
+		personMapper.Delete(delPerson)
+	*/
+
+	persons := personMapper.GetAll()
+
+	for _, person := range persons {
+		fmt.Println(person)
+	}
+
+	fmt.Println("===========================")
+
+	delPerson := personMapper.Get(9)
+	err = personMapper.Delete(delPerson)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	persons = personMapper.GetAll()
+
+	for _, person := range persons {
+		fmt.Println(person)
+	}
+
 }
